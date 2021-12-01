d300ac0edc25bec40f0cc271e077cc4cb92768ac
sbrevolution5 <sbrevolution5@aim.com>
2021-12-01 13:45:44 -0500

    Removed helloworld

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -2,6 +2,5 @@
 using AdventOfCode2021.Code;
 using AdventOfCode2021.inputs;
 
-Console.WriteLine("Hello, World!");
 Console.WriteLine(RadarReader.HowManyIncreases(Day1Input.Radar));
 Console.WriteLine(RadarReader.HowManySlidingIncreases(Day1Input.Radar));
