e7bdc166d2b98bf760f2223c427d0f88074883cf
sbrevolution5 <sbrevolution5@aim.com>
2021-12-02 08:51:50 -0500

    setup parser and object

---
+++ AdventOfCode2021/Code/SubmarineNavigation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Code
+{
+    internal static class SubmarineNavigation
+    {
+        public static int NavigateSub(string input)
+        {
+            var directions = Format(input);
+            return x * y;
+        }
+
+        private static List<Direction> Format(string input)
+        {
+            var lines = input.Split('\n');
+            var list = new List<Direction>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ');
+                Direction direction = new Direction();
+                if (parts[0] == "forward")
+                {
+                    direction.Command = Command.fwd;
+                }
+                else if (parts[0] == "down") { direction.Command = Command.down; }
+                else if (parts[0] == "up") { direction.Command = Command.up; }
+            }
+            return list;
+        }
+    }
+
+    internal class Direction
+    {
+        public Command Command { get; set; }
+        public int Number { get; set; }
+    }
+
+    public enum Command
+    {
+        fwd,
+        up,
+        down
+    }
+}
