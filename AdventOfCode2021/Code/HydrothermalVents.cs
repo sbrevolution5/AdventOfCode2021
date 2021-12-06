e27ab3fccdbe6fb5e06ab212e7ec3c727181e3f4
sbrevolution5 <sbrevolution5@aim.com>
2021-12-06 09:21:26 -0500

    add inputs and start 6

---
+++ AdventOfCode2021/Code/HydrothermalVents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Code
+{
+    public static class HydrothermalVents
+    {
+        public static int CountOverlaps(string input)
+        {
+
+            Grid grid = ParseInput(input);
+        }
+
+        private static Grid ParseInput(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Grid
+    {
+    }
+}
---
+++ AdventOfCode2021/Code/Lanternfish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Code
+{
+    internal static class Lanternfish
+    {
+        public static int HowManyFish(string input, int days)
+        {
+            var fish = ParseInput(input);
+        }
+
+        private static List<int> ParseInput(string input)
+        {
+            return input.Split(',').Select(i => int.Parse(i)).ToList();
+        }
+    }
+}
