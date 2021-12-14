eb75d2b01f6b304b7f808375c3667a31530cb8b6
sbrevolution5 <sbrevolution5@aim.com>
2021-12-14 12:23:31 -0500

    fix overlap

--- AdventOfCode2021/Code/Polymers.cs
+++ AdventOfCode2021/Code/Polymers.cs
@@ -8,6 +8,30 @@ namespace AdventOfCode2021.Code
 {
     public static class Polymers
     {
+        public static int GetLengthOfPolymerAfterSteps(string input, int steps)
+        {
+            string template = GetPolymerTemplate(input);
+            List<PolymerRule> rules = GetRules(input);
+            string res = "";
+            for (int i = 0; i < steps; i++)
+            {
+                res = PerformStep(template, rules);
+                template = res;
+            }
+            return template.Length;
+        }
+        public static string GetStringAfterSteps(string input, int steps)
+        {
+            string template = GetPolymerTemplate(input);
+            List<PolymerRule> rules = GetRules(input);
+            string res = "";
+            for (int i = 0; i < steps; i++)
+            {
+                res = PerformStep(template, rules);
+                template = res;
+            }
+            return template;
+        }
         public static int DifferenceInMostAndLeastCommon(string input)
         {
             string template = GetPolymerTemplate(input);
@@ -25,12 +49,14 @@ namespace AdventOfCode2021.Code
 
         private static int LeastCommonElement(string res)
         {
-            throw new NotImplementedException();
+            var grp = res.GroupBy(c => c);
+            return grp.Select(c => c.Count()).Min();
         }
 
         private static int MostCommonElement(string res)
         {
-            throw new NotImplementedException();
+            var grp = res.GroupBy(c => c);
+            return grp.Select(c => c.Count()).Max();
         }
 
         private static string PerformStep(string template, List<PolymerRule> rules)
@@ -43,6 +69,9 @@ namespace AdventOfCode2021.Code
                 List<InsertionInstruction> instructions = GetInsertionCommands(template, rule);
                 allInsertions.AddRange(instructions);
             }
+            allInsertions = allInsertions.OrderBy(i => i.i).ToList();
+            var  insertionPairStrings = allInsertions.Select(i => i.c + i.i.ToString()).ToArray();
+            var str = string.Join(" ", insertionPairStrings);
             var polymer = template;
             var insertionCount = 0;
             foreach (var instruction in allInsertions)
@@ -56,25 +85,64 @@ namespace AdventOfCode2021.Code
 
         private static string TransformWithInstruction(string polymer, InsertionInstruction instruction, int insertionCount)
         {
-            polymer.Insert(instruction.i+insertionCount,instruction.c.ToString());
-            return polymer;
+            //The problem is when there are multiple instructions, they aren't being inserted properly
+            //Why does it think there is a B at 6?!
+            var newPoly = polymer.Insert(instruction.i + insertionCount, instruction.c.ToString());
+            return newPoly;
         }
-
+        public static List<int> AllIndexesOf(string str, string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+            List<int> indexes = new List<int>();
+            for (int index = 0; ; index += value.Length-1)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
+        }
         private static List<InsertionInstruction> GetInsertionCommands(string template, PolymerRule rule)
         {
+            //We're missing an instruction when there are multiple matches
+            var originalTemplate = template;
             var commands = new List<InsertionInstruction>();
-            var offset = 0;
-            while (template.IndexOf(rule.Original) != -1)
+            var indexes = AllIndexesOf(template, rule.Original);
+            var ct = 0;
+            foreach (var pos in indexes)
             {
-                var currentIndex = template.IndexOf(rule.Original);
-                commands.Add(new()
+                commands.Add(new InsertionInstruction()
                 {
                     c = rule.Insert,
-                    i = currentIndex + offset
+                    i = pos + 1,
+                    Rule = rule,
                 });
-                offset += currentIndex;
-                template = template.Substring(offset);
             }
+            //var offset = 1;
+            //var removedCharacters = 0;
+            //var matchCount = 0;
+            ////Find pair in string
+            //while (template.IndexOf(rule.Original) != -1)//While we can still find original pair
+            //{
+            //    matchCount++;
+            //    //if its found, record the index we found it at
+            //    var currentIndex = template.IndexOf(rule.Original);
+            //    //create an instruction, that says "put this character at the index + 1, plus the number of removed characters"
+            //    var removalIndex = currentIndex + 1 + removedCharacters;
+            //    commands.Add(new()
+            //    {
+            //        c = rule.Insert,
+            //        i = removalIndex //if the pair is found at 0, this should be 1
+            //    });
+            //    //then remove the beginning of that string, so that we won't find the same pair
+            //    //update template to remove everything before the current
+            //    template = template.Substring(currentIndex + 1);
+            //    removedCharacters = +currentIndex + matchCount;
+            //    //Find pair again
+
+            //}
+            ////if not found, return the list of commands
             return commands;
         }
 
@@ -112,5 +180,6 @@ namespace AdventOfCode2021.Code
     {
         public char c { get; set; }
         public int i { get; set; }
+        public PolymerRule Rule { get; set; }
     }
 }
--- AdventOfCode2021Tests/Code/PolymersTests.cs
+++ AdventOfCode2021Tests/Code/PolymersTests.cs
@@ -5,16 +5,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace AdventOfCode2021.Code.Tests
 {
     [TestClass()]
     public class PolymersTests
     {
+        [DataTestMethod()]
+        [DataRow(1,7)]
+        [DataRow(2,13)]
+        [DataRow(3,25)]
+        [DataRow(4,49)]
+        [DataRow(5,97)]
+        [DataRow(10,3073)]
+        public void TestProperLength(int steps, int expected)
+        {
+            var input = @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+            var res = Polymers.GetLengthOfPolymerAfterSteps(input, steps);
+            res.Should().Be(expected);
+        }
+        [DataTestMethod()]
+        [DataRow(1, "NCNBCHB")]
+        [DataRow(2, "NBCCNBBBCBHCB")]
+        [DataRow(3, "NBBBCNCCNBBNBNBBCHBHHBCHB")]
+        [DataRow(4, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")]
+        public void TestProperString(int steps, string expected)
+        {
+            var input = @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+            var res = Polymers.GetStringAfterSteps(input, steps);
+            res.Should().Be(expected);
+        }
         [TestMethod()]
         public void DifferenceInMostAndLeastCommonTest()
         {
-            Assert.Fail();
+            var input = @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+            var res = Polymers.DifferenceInMostAndLeastCommon(input);
+            res.Should().Be(1588);
+        }
+        [TestMethod()]
+        public void DoubleMatchTest()
+        {
+            var input = @"NNCHNN
+
+NN -> B";
+            var res = Polymers.DifferenceInMostAndLeastCommon(input);
+            res.Should().Be(3);
         }
     }
 }
