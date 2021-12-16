fbc0bef84ff222d434af85ca97f18cadc3f95002
sbrevolution5 <sbrevolution5@aim.com>
2021-12-16 11:13:41 -0500

    Setup Day16 with tests

---
+++ AdventOfCode2021/Code/PacketDecoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Code
+{
+    public static class PacketDecoder
+    {
+        public static int GetSumOfVersionNumbers(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
